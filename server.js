58f451b3d441230cbe7d0bf160b69eda28c7753e
Mira Thoen Feiring <mirafeiring@gmail.com>
2016-08-23 15:36:26 +0200

    Delete outcommented code

--- server.js
+++ server.js
@@ -5,13 +5,8 @@ var jsonServer = require('json-server')
 
 var port = 3000;
 
-// app.use(express.static(__dirname)); // Current directory is root
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api', jsonServer.router(__dirname + '/db.json'));
-
-app.get('/', function(req, res){
-  res.send('hello world');
-});
 
 app.listen(port, function() {
     console.log('The tasks server has been started on port ' + port);
